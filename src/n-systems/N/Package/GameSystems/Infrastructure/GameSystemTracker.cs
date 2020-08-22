09d67a40523021d1b4df74f8f08f7bc29a2d9414
Doug <douglas.linder+git@gmail.com>
2020-08-22 12:59:00 +0800

    update deps, add preInitHook

--- src/n-systems/N/Package/GameSystems/Infrastructure/GameSystemTracker.cs
+++ src/n-systems/N/Package/GameSystems/Infrastructure/GameSystemTracker.cs
@@ -4,80 +4,97 @@ using Object = UnityEngine.Object;
 
 namespace N.Package.GameSystems.Infrastructure
 {
-  public class GameSystemTracker
-  {
-    private bool _currentState;
-
-    private GameSystem _instance;
-
-    public GameSystem Instance => _instance;
-
-    public bool Update(bool systemState, GameSystem prefab)
+    public class GameSystemTracker
     {
-      if (_currentState == systemState) return _currentState;
-      if (systemState)
-      {
-        SpawnAndInitializeSystem(prefab);
-      }
-      else
-      {
-        ShutdownAndDestroySystem();
-      }
+        private bool _currentState;
 
-      _currentState = systemState;
-      return _currentState;
+        private GameSystem _instance;
+
+        public GameSystem Instance => _instance;
+
+        public bool Update(bool systemState, GameSystem prefab)
+        {
+            return Update<GameSystem>(systemState, prefab, null);
+        }
+
+        public bool Update<T>(bool systemState, GameSystem prefab, Action<T> preInitHook) where T : class
+        {
+            if (_currentState == systemState) return _currentState;
+            if (systemState)
+            {
+                SpawnAndInitializeSystem(prefab, preInitHook);
+            }
+            else
+            {
+                ShutdownAndDestroySystem();
+            }
+
+            _currentState = systemState;
+            return _currentState;
+        }
+
+        private void ShutdownAndDestroySystem()
+        {
+            if (_instance == null) return;
+            try
+            {
+                _instance.Dispose();
+            }
+            catch (Exception error)
+            {
+                Debug.LogError($"Failed to shutdown game system: {_instance}: {error}");
+            }
+
+            try
+            {
+                Object.Destroy(_instance.gameObject);
+            }
+            catch (Exception error)
+            {
+                Debug.LogError($"Failed to destroy game system: {_instance}: {error}");
+                _instance = null;
+            }
+        }
+
+        private void SpawnAndInitializeSystem<T>(GameSystem prefab, Action<T> preInitHook) where T : class
+        {
+            try
+            {
+                ShutdownAndDestroySystem();
+                _instance = Object.Instantiate(prefab);
+                _instance.transform.name = _instance.GetType().Name;
+            }
+            catch (Exception error)
+            {
+                Debug.LogError($"Failed to create game system: {_instance}: {error}");
+                _instance = null;
+                return;
+            }
+
+            if (preInitHook != null)
+            {
+                try
+                {
+                    preInitHook(_instance as T);
+                }
+                catch (Exception error)
+                {
+                    Debug.LogError($"Failed to run preInitHook on game system: {_instance}: {error}");
+                }    
+            }
+
+            try
+            {
+                _instance.Initialize();
+            }
+            catch (Exception error)
+            {
+                Debug.LogError($"Failed to create game system: {_instance}: {error}");
+                ShutdownAndDestroySystem();
+                return;
+            }
+
+            Debug.Log($"Initialised game system: {_instance}");
+        }
     }
-
-    private void ShutdownAndDestroySystem()
-    {
-      if (_instance == null) return;
-      try
-      {
-        _instance.Dispose();
-      }
-      catch (Exception error)
-      {
-        Debug.LogError($"Failed to shutdown game system: {_instance}: {error}");
-      }
-
-      try
-      {
-        Object.Destroy(_instance.gameObject);
-      }
-      catch (Exception error)
-      {
-        Debug.LogError($"Failed to destroy game system: {_instance}: {error}");
-        _instance = null;
-      }
-    }
-
-    private void SpawnAndInitializeSystem(GameSystem prefab)
-    {
-      try
-      {
-        ShutdownAndDestroySystem();
-        _instance = Object.Instantiate(prefab);
-        _instance.transform.name = _instance.GetType().Name;
-      }
-      catch (Exception error)
-      {
-        Debug.LogError($"Failed to create game system: {_instance}: {error}");
-        _instance = null;
-        return;
-      }
-
-      try
-      {
-        _instance.Initialize();
-      }
-      catch (Exception error)
-      {
-        Debug.LogError($"Failed to create game system: {_instance}: {error}");
-        ShutdownAndDestroySystem();
-        return;
-      }
-
-      Debug.Log($"Initialised game system: {_instance}");
-    }
-  }
 }
--- src/n-systems/N/Package/GameSystems/SystemsManager.cs
+++ src/n-systems/N/Package/GameSystems/SystemsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using N.Package.Bind;
@@ -7,55 +8,67 @@ using UnityEngine;
 
 namespace N.Package.GameSystems
 {
-  public abstract class SystemsManager : MonoBehaviour
-  {
-    public List<GameSystemReference> Systems;
-
-    private Registry _services;
-
-    private AtInterval _updateSystemsAtInterval;
-
-    private void Awake()
+    public abstract class SystemsManager : MonoBehaviour
     {
-      _updateSystemsAtInterval = new AtInterval()
-      {
-        Interval = 0.1f,
-        Action = SyncEnabledSystems
-      };
+        public List<GameSystemReference> Systems;
 
-      SyncEnabledSystems();
+        private Registry _services;
+
+        private AtInterval _updateSystemsAtInterval;
+
+        private void Awake()
+        {
+            _updateSystemsAtInterval = new AtInterval()
+            {
+                Interval = 0.1f,
+                Action = SyncEnabledSystems
+            };
+
+            SyncEnabledSystems();
+        }
+
+        private void SyncEnabledSystems()
+        {
+            Systems.ForEach(i => i.Exists = i.Tracker.Update(i.Enabled, i.Template));
+        }
+
+        public void Update()
+        {
+            _updateSystemsAtInterval.Update(Time.deltaTime);
+        }
+
+        /// <summary>
+        /// Explicitly initialize a system by catching the instance before the Initialize method is called.
+        /// You can use this to override the state of system before it is initialized, eg. for tests.
+        /// Note this is a one time action; disabling and enabling the system will skip this.
+        /// </summary>
+        public void Initialize<T>(Action<T> preInitHook) where T : GameSystem
+        {
+            Systems.Where(i => i.Template.GetType() == typeof(T))
+                .ToList()
+                .ForEach(system => { system.Tracker.Update(system.Enabled, system.Template, preInitHook); });
+        }
+
+        protected abstract IServiceModule Services();
+
+        public Option<T> System<T>() where T : GameSystem
+        {
+            var systemRef = Systems.FirstOrDefault(i => i.Exists && i.Tracker.Instance.GetType() == typeof(T));
+            return systemRef == null ? Option.None<T>() : Option.Some(systemRef.Tracker.Instance as T);
+        }
+
+        /// <summary>
+        /// Return the registry to resolve service dependencies.
+        /// </summary>
+        public Registry Registry
+        {
+            get
+            {
+                if (_services != null) return _services;
+                _services = new Registry();
+                _services.Register(Services());
+                return _services;
+            }
+        }
     }
-
-    private void SyncEnabledSystems()
-    {
-      Systems.ForEach(i => i.Exists = i.Tracker.Update(i.Enabled, i.Template));
-    }
-
-    public void Update()
-    {
-      _updateSystemsAtInterval.Update(Time.deltaTime);
-    }
-
-    protected abstract IServiceModule Services();
-
-    public Option<T> System<T>() where T : GameSystem
-    {
-      var systemRef = Systems.FirstOrDefault(i => i.Exists && i.Tracker.Instance.GetType() == typeof(T));
-      return systemRef == null ? Option.None<T>() : Option.Some(systemRef.Tracker.Instance as T);
-    }
-
-    /// <summary>
-    /// Return the registry to resolve service dependencies.
-    /// </summary>
-    public Registry Registry
-    {
-      get
-      {
-        if (_services != null) return _services;
-        _services = new Registry();
-        _services.Register(Services());
-        return _services;
-      }
-    }
-  }
 }
