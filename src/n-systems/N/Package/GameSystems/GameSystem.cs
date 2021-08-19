215ee74bc08b419108b966468cbfbe4babf03a2b
Doug <douglas.linder+git@gmail.com>
2021-08-19 16:40:44 +0800

    adds helper functions

--- src/n-systems/N/Package/GameSystems/GameSystem.cs
+++ src/n-systems/N/Package/GameSystems/GameSystem.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 namespace N.Package.GameSystems
 {
-  public abstract class GameSystem : MonoBehaviour
-  {
-    /// <summary>
-    /// Initialize the state of this system from the current scene.
-    /// If we have any persistent state, reset it.
-    /// </summary>
-    public abstract void Initialize();
+    public abstract class GameSystem : MonoBehaviour
+    {
+        /// <summary>
+        /// Initialize the state of this system from the current scene.
+        /// If we have any persistent state, reset it.
+        /// </summary>
+        public abstract void Initialize();
 
-    /// <summary>
-    /// Shutdown this system.
-    /// </summary>
-    public abstract void Dispose();
-  }
+        /// <summary>
+        /// Shutdown this system.
+        /// </summary>
+        public abstract void Dispose();
+    }
 }
--- src/n-systems/N/Package/GameSystems/LazySystem.cs
+++ src/n-systems/N/Package/GameSystems/LazySystem.cs
@@ -1,34 +1,56 @@
+using System;
+using System.Threading.Tasks;
 using N.Package.Optional;
 
 namespace N.Package.GameSystems
 {
-  /// <summary>
-  /// Helper to lazy load services for consumers with no constructor (eg. MonoBehaviour).
-  /// </summary>
-  public struct LazySystem<T> where T : GameSystem
-  {
-    private bool _initialized;
-
-    private Option<T> _instance;
-
-    public Option<T> Value
+    /// <summary>
+    /// Helper to lazy load services for consumers with no constructor (eg. MonoBehaviour).
+    /// </summary>
+    public struct LazySystem<T> where T : GameSystem
     {
-      get
-      {
-        if (_initialized)
+        private bool _initialized;
+
+        private Option<T> _instance;
+
+        public Option<T> Value
         {
-          return _instance;
+            get
+            {
+                if (_initialized)
+                {
+                    return _instance;
+                }
+
+                _instance = Systems.System<T>();
+                _initialized = true;
+                return _instance;
+            }
         }
 
-        _instance = Systems.System<T>();
-        _initialized = true;
-        return _instance;
-      }
-    }
+        public void Reset()
+        {
+            _initialized = false;
+        }
 
-    public void Reset()
-    {
-      _initialized = false;
+        public bool With(Action<T> action)
+        {
+            return Value.With<T>(action);
+        }
+
+        public Task<bool> WithAsync(Func<T, Task> action)
+        {
+            return Value.WithAsync(action);
+        }
+
+        public TRtn WithValue<TRtn>(Func<T, TRtn> action, TRtn defaultValue)
+        {
+            return Value.WithValue(action, defaultValue);
+        }
+
+        public Task<TRtn> WithValueAsync<TRtn>(Func<T, Task<TRtn>> action, TRtn defaultValue)
+        {
+            return Value.WithValueAsync(action, defaultValue);
+        }
     }
-  }
 }
